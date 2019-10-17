0635da0a8e043461f03561c35ef4b1d12c7ee9e8
jasony62 <jasony62@qq.com>
2019-10-17 14:44:01 +0800

    update code

---
+++ ue/src/components/Editor.vue
@@ -0,0 +1,43 @@
+<template>
+  <el-dialog title="文件信息" :visible="dialogVisible" @close="onClose">
+    <tms-form :schemas="schemas" :data="file"></tms-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="onSubmit">取消</el-button>
+      <el-button type="primary" @click="onSubmit">保存修改</el-button>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import Vue from 'vue'
+import { Dialog, Button } from 'element-ui'
+Vue.use(Dialog).use(Button)
+
+import { TmsForm } from 'tms-form-vant/lib/form'
+
+export default {
+  components: { TmsForm },
+  props: {
+    schemas: { type: Array },
+    file: { type: Object }
+  },
+  data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  created() {
+    this.$on('open', function() {
+      this.dialogVisible = true
+    })
+  },
+  methods: {
+    onClose() {
+      this.dialogVisible = false
+    },
+    onSubmit() {
+      this.onClose()
+    }
+  }
+}
+</script>

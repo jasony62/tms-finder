177f90a6aae377a6164f61c72f17e9ba8a514a54
liao1a <153887111@qq.com>
2019-10-31 11:41:21 +0800

    添加忽略

--- back/config/config.js
+++
@@ -1,6 +0,0 @@
-let config = {}
-// config.APP_PROTOCOL = (configCus.APP_PROTOCOL) ? configCus.APP_PROTOCOL : 'http://'
-// config.APP_HTTP_HOST = (configCus.APP_HTTP_HOST) ? configCus.APP_HTTP_HOST : 'localhost/ue'
-config.APP_PASSWORD_ERROR_MAXNUM = 3
-
-module.exports = config

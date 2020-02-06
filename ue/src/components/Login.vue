c14d738864bd03ed2b4093b2e10634158a668868
jasony62 <jasony62@qq.com>
2020-02-06 17:26:37 +0800

    优化代码

--- ue/src/components/Login.vue
+++ ue/src/components/Login.vue
@@ -11,7 +11,7 @@ export default {
       if (this.$tmsRouterHistory.canBack()) {
         this.$router.back()
       } else {
-        this.$router.push('/')
+        this.$router.push({name:'root'})
       }
     }
   }

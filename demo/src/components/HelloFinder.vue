43142076744c2f7046b88961f7b52ab46217df9c
董河壮 <donghezhuang1995@gmail.com>
2020-04-01 14:45:12 +0800

    更改iframe弹框大小

--- demo/src/components/HelloFinder.vue
+++ demo/src/components/HelloFinder.vue
@@ -2,9 +2,10 @@
   <div class="hello">
     <el-button type="text" @click="dialogVisible = true">打开tms-finder选择文件</el-button>
     <div>返回结果：{{returnData}}</div>
-    <el-dialog title="双击选择返回结果" :visible.sync="dialogVisible" width="90%">
-      <div>
+    <el-dialog class="abow_dialog" title="双击选择返回结果" :visible.sync="dialogVisible" width="90%" top="4vh">
+      <div class="iframe-container">
         <iframe
+          id="iframe"
           width="100%"
           height="100%"
           frameborder="0"
@@ -12,6 +13,7 @@
           marginwidth="0"
           marginheight="0"
           scrolling="no"
+          @load="iframeLoad"
         ></iframe>
       </div>
     </el-dialog>
@@ -24,7 +26,7 @@ export default {
   data() {
     return {
       dialogVisible: false,
-      url: 'http://localhost:8080/web/storage?domain=download',
+      url: 'http://localhost:8080/finder_ue/web/storage',
       returnData: ''
     }
   },
@@ -40,6 +42,36 @@ export default {
       },
       false
     )
+  },
+  methods: {
+    iframeLoad(){
+      var ifm = document.getElementById("iframe"); 
+      ifm.height = document.documentElement.clientHeight;
+    }
   }
 }
 </script>
+<style>
+  .abow_dialog {
+    display: flex;
+    justify-content: center;
+    align-items: Center;
+    overflow: hidden;
+  }
+  .abow_dialog .el-dialog {
+    margin: 0 auto !important;
+    height: 90%;
+    overflow: hidden;
+  }
+  .abow_dialog .el-dialog__body {
+    position: absolute;
+    left: 0;
+    top: 54px;
+    bottom: 0;
+    right: 0;
+    padding: 0;
+    z-index: 1;
+    overflow: hidden;
+    overflow-y: auto;
+  }
+</style>

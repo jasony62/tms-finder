e2ab64005b584d15a572e82ce046b7294071ec5c
董河壮 <donghezhuang1995@gmail.com>
2020-04-13 20:22:28 +0800

    update code

--- ue/src/apis/file/browse.js
+++ ue/src/apis/file/browse.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const baseApi = (process.env.VUE_APP_API_SERVER || '') + '/file/browse'
 
 export default function create(tmsAxios) {
@@ -11,29 +10,11 @@ export default function create(tmsAxios) {
       if (domain !== undefined) params.domain = domain
       if (bucket !== undefined) params.bucket = bucket
       return tmsAxios.get(`${baseApi}/list`, { params }).then(rst => {
-=======
-import { TmsAxios } from 'tms-vue'
-const base = '/finder/api/file/browse';
-const baseUe = '/finder/ue/auth';
-
-export default {
-  schemas() {
-    return TmsAxios.ins('file-api')
-      .get(`${base}/schemas`)
-      .then(rst => rst.data.result)
-      .catch(err => Promise.reject(err))
-  },
-  list(dirName = '') {
-    return TmsAxios.ins('file-api')
-      .get(`${base}/list?dir=${dirName}`)
-      .then(rst => {
->>>>>>> c70120cb6e54deb614ec8d8ecc02eb5b9e73c28c
         rst.data.result.files.forEach(f => {
           if (typeof f.info !== 'object') f.info = {}
         })
         return rst.data.result
       })
-<<<<<<< HEAD
     },
     setInfo(path, info, domain, bucket) {
       const params = { path, domain, bucket }
@@ -42,32 +23,5 @@ export default {
     overallSearch(params) {
       return tmsAxios.post(`${baseApi}/listAll`, params).then(rst => rst.data.result)
     }
-=======
-      .catch(err => Promise.reject(err))
-  },
-  setInfo(path, info) {
-    return TmsAxios.ins('file-api')
-      .post(`${base}/setInfo?path=${path}`, info)
-      .then(rst => rst.data.result)
-      .catch(err => Promise.reject(err))
-  },
-  overallSearch(params) {
-    return TmsAxios.ins('file-api')
-    .post(`${base}/listAll`, params)
-    .then(rst => rst.data.result)
-    .catch(err => Promise.reject(err))
-  },
-  fnGetCaptcha() {
-    return TmsAxios.ins('file-api')
-    .post(`${baseUe}/captcha`)
-    .then(rst => Promise.resolve(rst.data))
-    .catch(err => Promise.resolve(err))
-  },
-  fnGetToken(params) {
-    return TmsAxios.ins('file-api')
-    .post(`${baseUe}/token`, params)
-    .then(rst => Promise.resolve(rst.data))
-    .catch(err => Promise.resolve(err))
->>>>>>> c70120cb6e54deb614ec8d8ecc02eb5b9e73c28c
   }
 }

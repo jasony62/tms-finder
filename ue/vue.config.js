22d56084d7fa136b1f31ebb8ba877994ea68911e
董河壮 <donghezhuang1995@gmail.com>
2020-05-21 10:54:25 +0800

    update code

--- ue/vue.config.js
+++ ue/vue.config.js
@@ -2,5 +2,19 @@ const VUE_APP_BASE_URL = process.env.VUE_APP_BASE_URL ? process.env.VUE_APP_BASE
 
 module.exports = {
   publicPath: `${VUE_APP_BASE_URL}/web`,
-  outputDir: `dist${VUE_APP_BASE_URL}/web`
+  outputDir: `dist${VUE_APP_BASE_URL}/web`,
+  devServer: {
+    proxy: {
+      // proxy: target
+      "/finder/auth": {
+        target: process.env.VUE_APP_API_HOST,
+      },
+      "/finder/api": {
+        target: process.env.VUE_APP_API_HOST,
+      },
+      "/finder/fs": {
+        target: process.env.VUE_APP_API_HOST,
+      },
+    }
+  }
 }

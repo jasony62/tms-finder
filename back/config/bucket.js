ba6f3fc2c3f2d31809c1929a77d29a2f1b4debcf
DESKTOP-MP4H2RS\baiss <2764411477@qq.com>
2022-06-01 20:36:17 +0800

    config/bucket文件修正取值

--- back/config/bucket.js
+++ back/config/bucket.js
@@ -1,6 +1,6 @@
 /**
  * bucket处理获取bucket值，相关的逻辑
  */
-module.exports = function (client) {
-  return [true, client.bucket]
+module.exports = function (ctx, client) {
+  return [true, client.data.bucket]
 }

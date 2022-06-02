e6b59bd36411d3c9e3b3e1f2a98f2830699dd4db
DESKTOP-MP4H2RS\baiss <2764411477@qq.com>
2022-06-02 18:14:32 +0800

    更新依赖包版本以及配置文件的代码

--- back/config/app.js
+++ back/config/app.js
@@ -10,6 +10,7 @@ let appConfig = {
   router: {
     auth: {
       prefix: 'auth', // 鉴权接口调用url的前缀
+      plugins_npm: [{ id: 'tms-koa-account', alias: 'account' }], // 账号管理相关接口
     },
     controllers: {
       prefix: 'api', // 接口调用url的前缀
@@ -24,6 +25,14 @@ let appConfig = {
       privateKey: 'tms-finder-secret',
       expiresIn: 3600,
     },
+    // 鉴权 redis  如果鉴权信息要从redis中获取，可在此连接redis需注释上面jwt对象，redis中端口、地址等信息可在项目根目录下创建/back/.env文件配置成环境变量，并且需要在/back/config/文件夹中依据redis.sample.js创建redis.js
+    // redis: {
+    //   prefix: process.env.TMS_REDIS_PREFIX || "tms-mongodb-web",
+    //   host: process.env.TMS_REDIS_HOST || "localhost",
+    //   port: parseInt(process.env.TMS_REDIS_PORT) || 6379,
+    //   password: process.env.TMS_REDIS_PWD || '',
+    //   expiresIn: parseInt(process.env.TMS_REDIS_EXPIRESIN) || 7200
+    // },
     // 验证码
     captcha: {
       // 如需自定义验证码，可在项目根目录下创建生成验证码文件，将文件路径命名给path并注释code
@@ -37,21 +46,6 @@ let appConfig = {
         checker: 'checkCaptcha',
         generator: 'createCaptcha',
       },
-    },
-    //
-    client: {
-      // 如需自定义鉴权函数，可在项目根目录下创建鉴权函数, 将文件路径命名给path并注释 accounts
-      // path: "files"
-      accounts: [
-        // 默认用户组
-        {
-          id: 1,
-          bucket_id: 1,
-          bucket: 'ctsi',
-          username: 'root',
-          password: 'root',
-        },
-      ],
     },
     /* 检查bucket参数，支持多租户访问 */
     bucket: { validator: './config/bucket.js' },

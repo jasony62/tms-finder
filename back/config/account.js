7ec4c41f1d3a2a2c616c2c2bfcc05bdfdbbe1b62
DESKTOP-MP4H2RS\baiss <2764411477@qq.com>
2022-06-02 16:38:57 +0800

    只是代码格式化

--- back/config/account.js
+++ back/config/account.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   redis: {
     disabled: true,
-    name: 'master'
+    name: 'master',
   },
   // accounts: [
   //   {
@@ -22,23 +22,23 @@ module.exports = {
   // ],
   admin: { username: 'admin', password: 'admin' },
   accountBeforeEach: (ctx) => {
-    const crypto = require("crypto")
+    const crypto = require('crypto')
     const authDecode = (string, key) => {
-      const cipher = crypto.createDecipheriv("aes-128-cbc", key, key)
-      let decrypted = cipher.update(string, "hex", "utf8")
-      decrypted += cipher.final("utf8")
+      const cipher = crypto.createDecipheriv('aes-128-cbc', key, key)
+      let decrypted = cipher.update(string, 'hex', 'utf8')
+      decrypted += cipher.final('utf8')
       return decrypted
     }
     const getPwd = (seed) => {
-      const md5 = crypto.createHash("md5")
-      return md5.update(seed).digest("hex")
+      const md5 = crypto.createHash('md5')
+      return md5.update(seed).digest('hex')
     }
 
     let { username, password } = ctx.request.body
     if (ctx.request.query.adc) {
-      const authEncryKey = ctx.request.query.adc + "adc"
+      const authEncryKey = ctx.request.query.adc + 'adc'
       if (authEncryKey.length !== 16) {
-        return Promise.reject("参数错误2")
+        return Promise.reject('参数错误2')
       }
       username = authDecode(username, authEncryKey)
       password = authDecode(password, authEncryKey)
@@ -61,17 +61,17 @@ module.exports = {
   // },
   authConfig: {
     pwdErrMaxNum: 5, // int 密码错误次数限制 0 不限制
-    authLockDUR: 300,   // int 登录锁定时长 （秒）
+    authLockDUR: 300, // int 登录锁定时长 （秒）
     pwdStrengthCheck: {
       min: 8, // 密码最小长度
       max: 20, // 密码最大长度
-      pwdBlack: ["P@ssw0rd"], // 密码黑名单
-      containProjects: { mustCheckNum: 3, contains: ["digits", "uppercase", "lowercase", "symbols"] }, // 是否包含数字、大写字母、小写字母、特殊字符, 至少满足其中length项
+      pwdBlack: ['P@ssw0rd'], // 密码黑名单
+      containProjects: { mustCheckNum: 3, contains: ['digits', 'uppercase', 'lowercase', 'symbols'] }, // 是否包含数字、大写字母、小写字母、特殊字符, 至少满足其中length项
       hasSpaces: false, // 是否包含空格
       hasAccount: false,
       hasKeyBoardContinuousChar: false,
       // hasKeyBoardContinuousCharSize: 4
-    }
+    },
   },
   // captchaConfig: {
   //   disabled: false,   // boolean 是否启用验证码

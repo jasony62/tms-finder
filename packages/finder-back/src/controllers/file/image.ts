b51e12d528a50b4102fe3493587729f15925330f
quhua <quhuame@163.com>
2022-07-12 15:39:27 +0800

    初始版本

---
+++ packages/finder-back/src/controllers/file/image.ts
@@ -0,0 +1,10 @@
+import { ImageCtrl } from 'tms-koa/lib/controller/fs/image'
+
+/** 图片文件控制器 */
+class Image extends ImageCtrl {
+  constructor(...args) {
+    super(...args)
+  }
+}
+
+export default Image
